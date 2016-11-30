0a8ac6c65780b46a88d95db080f470c5021ab26e
dustinrue <ruedu@dustinrue.com>
2016-11-29 21:35:32 -0600

    initial

--- Makefile
+++ Makefile
@@ -1,10 +1,6 @@
-all: push
+all: run
 
 build:
-	docker build -t apache24-php70 .
-
-tag: build
-	docker tag apache24-php70 fccorg/apache24-php70
-
-push: tag
-	docker push fccorg/apache24-php70
+	docker build -t php70-memcached-builder .
+run: build
+	docker-compose up && docker-compose down
--- lib/run.sh
+++ lib/run.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-#chown www-data:www-data /var/www/html -R
-
-if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
-    unset ALLOW_OVERRIDE
-else
-    sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
-    a2enmod rewrite
-fi
-
-rm -f /var/run/apache2/apache2.pid
-source /etc/apache2/envvars
-apachectl configtest
-tail -F /var/log/apache2/* &
-exec apache2 -D FOREGROUND
+git clone https://github.com/php-memcached-dev/php-memcached.git
+cd php-memcached
+git checkout php7
+phpize
+./configure
+make
+make install
+cp /usr/lib/php/*/memcached.so /output/memcached.so
